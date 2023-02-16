dc9bd8c562752870b43b77c8516324e2c4774825
Nursultan <nursultan14n@gmail.com>
2023-02-16 15:58:35 +0600

    1

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 let container = document.querySelector(".container");
+
 let API = "https:www.omdbapi.com/?apikey=b6003d8a&s=All";
 console.log(API);
-console.log(23);
+console.log(container);
 
 // fetch(API);
 fetch(API)
   .then((data) => data.json())
-
   .then((res) => {
-    console.log(1);
     res.Search.forEach((elem) => {
-      container.innerHTML += `<div class='card'><img src=${elem.Poster}class='movie-img' >  <h2 class='movie-title'>${elem.Title}</h2> <p class='movie-year'>Year
-      ${elem.Year}</p> </div>`;
+      container.innerHTML += `
+      <div class='card'>
+        <img src=${elem.Poster} class='movie-img'>
+        <h2 class='movie-title'>${elem.Title}</h2>
+        <p class='movie-year'>Year${elem.Year}</p>
+      </div>`;
     });
   });
 // let description = document.querySelector("#description");
